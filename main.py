05abcdd02c1ddd3a5fa3a38403ba1fbd69f1bfba
alexh500 <86416389+alexh500@users.noreply.github.com>
2022-03-01 21:07:36 +0000

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -24,6 +25,12 @@ player2X = 980
 player2Y = 450
 player2Y_change = 0
 
+circleX = 500
+circleY = random.randint(5, 745)
+circle_radius = 10
+velX = 1 if random.randint(1, 2) == 1 else -1
+velY = 1 if random.randint(1, 2) == 1 else -1
+
 
 def show_score(x1, y, x2):
     left_score = scoreL.render(str(score_left), True, (255, 255, 255))
@@ -43,6 +50,10 @@ def player(x1, y1, x2, y2):
     screen.blit(playerImg, (x2, y2))
 
 
+def make_circle(x, y, r):
+    pygame.draw.circle(screen, (255, 255, 255), (x, y), r)
+
+
 running = True
 while running:
     screen.fill((0, 0, 0))
@@ -51,13 +62,13 @@ while running:
             running = False
         if ev.type == pygame.KEYDOWN:
             if ev.key == pygame.K_w:
-                player1Y_change = -0.5
+                player1Y_change = -0.4
             if ev.key == pygame.K_s:
-                player1Y_change = 0.5
+                player1Y_change = 0.4
             if ev.key == pygame.K_UP:
-                player2Y_change = -0.5
+                player2Y_change = -0.4
             if ev.key == pygame.K_DOWN:
-                player2Y_change = 0.5
+                player2Y_change = 0.4
         if ev.type == pygame.KEYUP:
             if ev.key == pygame.K_s or ev.key == pygame.K_w:
                 player1Y_change = 0
@@ -75,6 +86,39 @@ while running:
         player2Y = 0
     elif player2Y >= 650:
         player2Y = 650
+
+    make_circle(circleX, circleY, circle_radius)
+    if velX > 0:
+        circleX += 0.2
+    elif velX < 0:
+        circleX -= 0.2
+    if velY > 0:
+        circleY += 0.2
+    elif velY < 0:
+        circleY -= 0.2
+
+    if circleY <= 10:
+        velY *= -1
+    elif circleY >= 740:
+        velY *= -1
+
+    if circleX <= 37 and player1Y <= circleY <= player1Y + 100 or circleX >= 963 and player2Y <= circleY<=player2Y+100:
+        velX *= -1
+
+    if circleX <= 10:
+        score_right += 1
+        circleX = 500
+        circleY = random.randint(5, 745)
+        circle_radius = 10
+        velX = 1 if random.randint(1, 2) == 1 else -1
+        velY = 1 if random.randint(1, 2) == 1 else -1
+    elif circleX >= 990:
+        score_left += 1
+        circleX = 500
+        circleY = random.randint(5, 745)
+        circle_radius = 10
+        velX = 1 if random.randint(1, 2) == 1 else -1
+        velY = 1 if random.randint(1, 2) == 1 else -1
 
     player(player1X, player1Y, player2X, player2Y)
     show_score(scoreLX, scoreY, scoreRX)
